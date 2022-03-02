4e608fb9aa9d2f3d58b2c94f57e93777784679e7
khanmdsohel02 <khanmdsohel02.25@gmail.com>
2022-03-02 10:38:04 +0600

    api fetch

---
+++ script.js
@@ -0,0 +1,10 @@
+const allMobile =( ) =>{
+    const searchValue =document.getElementById("search-box").value;
+
+    const url ='https://openapi.programming-hero.com/api/phones?search=${searchValue}'
+    fetch(url)
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+
+    
+}
